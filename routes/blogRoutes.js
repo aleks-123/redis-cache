156f6fde162239d3415bd7e4d168d0f8aba1d240
aleks-123 <ace.ki@hotmail.com>
2023-09-20 19:48:42 +0200

    third commit

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,31 +1,51 @@
-const mongoose = require('mongoose');
-const requireLogin = require('../middlewares/requireLogin');
+const mongoose = require("mongoose");
+const requireLogin = require("../middlewares/requireLogin");
 
-const Blog = mongoose.model('Blog');
+const Blog = mongoose.model("Blog");
 
-module.exports = app => {
-  app.get('/api/blogs/:id', requireLogin, async (req, res) => {
+module.exports = (app) => {
+  app.get("/api/blogs/:id", requireLogin, async (req, res) => {
     const blog = await Blog.findOne({
       _user: req.user.id,
-      _id: req.params.id
+      _id: req.params.id,
     });
 
     res.send(blog);
   });
 
-  app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
+  app.get("/api/blogs", requireLogin, async (req, res) => {
+    // const redis = require("redis");
+    // const util = require("util");
+    // const redisUrl = "redis://127.0.0.1:6379";
+    // const client = redis.createClient(redisUrl);
 
+    // client.get = util.promisify(client.get);
+
+    // // do we have any cached data in redis related to this query
+    // const canchedBlogs = await client.get(req.user.id);
+    // //if yes, then respond to ther request right awway and return
+
+    // if (canchedBlogs) {
+    //   console.log("SERVING FROM CACHE");
+    //   return res.send(JSON.parse(canchedBlogs));
+    // }
+    // //if no, we need to respond to request and update our cache to store the data
+
+    const blogs = await Blog.find({ _user: req.user.id }).cache();
+
+    // console.log("SERVING FROM MONGODB");
     res.send(blogs);
+
+    // client.set(req.user.id, JSON.stringify(blogs));
   });
 
-  app.post('/api/blogs', requireLogin, async (req, res) => {
+  app.post("/api/blogs", requireLogin, async (req, res) => {
     const { title, content } = req.body;
 
     const blog = new Blog({
       title,
       content,
-      _user: req.user.id
+      _user: req.user.id,
     });
 
     try {
